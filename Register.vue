1885cc5dfc5e5949942ef309571f2039380a59e5
GabrielCCruz <106630465+GabrielCCruz@users.noreply.github.com>
2024-07-17 21:29:57 -0300

    Update Register.vue

--- Register.vue
+++ Register.vue
@@ -68,8 +68,46 @@ export default {
 </script>
 
 <style scoped>
-.error-message {
-  color: red;
-  margin-top: 1em;
+.register {
+  max-width: 400px;
+  margin: auto;
+  padding: 2em;
+  border: 1px solid #ccc;
+  border-radius: 1em;
+  background-color: #f7f7f7;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+.register h2 {
+  color: #4267B2; /* Cor do título inspirado no Facebook */
+  text-align: center;
+}
+.form-group {
+  margin-bottom: 1em;
+}
+label {
+  margin-bottom: .5em;
+  color: #333333;
+  display: block;
+}
+input {
+  border: 1px solid #CCCCCC;
+  padding: .5em;
+  font-size: 1em;
+  width: 100%;
+  box-sizing: border-box;
+  border-radius: 0.3em;
+}
+button {
+  padding: 0.7em;
+  color: #fff;
+  background-color: #4267B2; /* Cor do botão inspirado no Facebook */
+  border: none;
+  border-radius: 0.3em;
+  cursor: pointer;
+  width: 100%;
+  font-size: 1em;
+}
+button:hover {
+  background-color: #365899; /* Cor do botão ao passar o mouse inspirado no Facebook */
 }
 </style>
